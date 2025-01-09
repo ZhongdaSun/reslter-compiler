b35cb45e151c65fe91ecaf4f4f9c80d9c4a2f32e
zhongdasun <sunzhongdasd@gmail.com>
2025-01-09 15:43:15 +0800

    test pass test_simple_crud_api in test_annotation module.

---
+++ atest/compiler_atest/baseline/test_annotation/simple_crud_api/grammar.py
@@ -0,0 +1,117 @@
+""" THIS IS AN AUTOMATICALLY GENERATED FILE!"""
+from __future__ import print_function
+import json
+from restler.engine import primitives
+from restler.engine.core import requests
+from restler.engine.errors import ResponseParsingException
+from restler.engine import dependencies
+
+_products_post_version = dependencies.DynamicVariable("_products_post_version")
+
+def parse_productspost(data, **kwargs):
+    """ Automatically generated response parser """
+    # Declare response variables
+    temp_7262 = None
+
+    if 'headers' in kwargs:
+        headers = kwargs['headers']
+
+
+    # Parse body if needed
+    if data:
+
+        try:
+            data = json.loads(data)
+        except Exception as error:
+            raise ResponseParsingException("Exception parsing response, data was not valid json: {}".format(error))
+        pass
+
+    # Try to extract each dynamic object
+
+        try:
+            temp_7262 = str(data["version"])
+            
+        except Exception as error:
+            # This is not an error, since some properties are not always returned
+            pass
+
+
+
+    # If no dynamic objects were extracted, throw.
+    if not (temp_7262):
+        raise ResponseParsingException("Error: all of the expected dynamic objects were not present in the response.")
+
+    # Set dynamic variables
+    if temp_7262:
+        dependencies.set_variable("_products_post_version", temp_7262)
+
+req_collection = requests.RequestCollection([])
+# Endpoint: /products, method: Post
+request = requests.Request([
+    primitives.restler_static_string("POST "),
+    primitives.restler_basepath("/api"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("products"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: localhost:8888\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+    
+    {
+
+        'post_send':
+        {
+            'parser': parse_productspost,
+            'dependencies':
+            [
+                _products_post_version.writer()
+            ]
+        }
+
+    },
+
+],
+requestId="/products"
+)
+req_collection.add_request(request)
+
+# Endpoint: /services, method: Put
+request = requests.Request([
+    primitives.restler_static_string("PUT "),
+    primitives.restler_basepath("/api"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("services"),
+    primitives.restler_static_string("?"),
+    primitives.restler_static_string("product-ver="),
+    primitives.restler_static_string(_products_post_version.reader(), quoted=False),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: localhost:8888\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/services"
+)
+req_collection.add_request(request)
+
+# Endpoint: /services, method: Get
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_basepath("/api"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("services"),
+    primitives.restler_static_string("?"),
+    primitives.restler_static_string("product-ver="),
+    primitives.restler_fuzzable_string("fuzzstring", quoted=False),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: localhost:8888\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/services"
+)
+req_collection.add_request(request)
