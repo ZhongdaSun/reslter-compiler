2cf18fb536b89e2e01aa76a20bbc0cfcfc6edc8d
zhongdasun <sunzhongdasd@gmail.com>
2025-01-09 17:17:56 +0800

    test pass test_simple_swagger_all_param_types in test_dict module.

--- atest/compiler_atest/baseline/test_dict/simple_swagger_all_param_types/grammar.py
+++ atest/compiler_atest/baseline/test_dict/simple_swagger_all_param_types/grammar.py
@@ -1,10 +1,10 @@
 """ THIS IS AN AUTOMATICALLY GENERATED FILE!"""
 from __future__ import print_function
 import json
-from restler.engine import primitives
-from restler.engine.core import requests
-from restler.engine.errors import ResponseParsingException
-from restler.engine import dependencies
+from engine import primitives
+from engine.core import requests
+from engine.errors import ResponseParsingException
+from engine import dependencies
 
 _customer_post_id = dependencies.DynamicVariable("_customer_post_id")
 
@@ -69,7 +69,7 @@ request = requests.Request([
     primitives.restler_static_string("{"),
     primitives.restler_static_string("""
     "id":"""),
-    primitives.restler_fuzzable_string("fuzzstring", quoted=True),
+    primitives.restler_custom_payload("id", quoted=True),
     primitives.restler_static_string(""",
     "Person":
         {
